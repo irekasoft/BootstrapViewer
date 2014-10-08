d7acff63cb8939132e666cb85c5732c4a8426e53
Muhammad Hijazi <irekasoft@gmail.com>
2014-10-08 21:45:18 +0800

    Initial Commit

---
+++ BootstrapViewer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BootstrapViewer
+//
+//  Created by Hijazi on 10/8/14.
+//  Copyright (c) 2014 iReka Soft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BootstrapViewer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BootstrapViewer
+//
+//  Created by Hijazi on 10/8/14.
+//  Copyright (c) 2014 iReka Soft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
